41b7dce7b90d2cca25de14e7e53df7ecb91fdc24
Axeezz <al.sanko06@tut.by>
2025-02-24 10:59:05 +0300

    .

--- src/main/java/com/movio/moviolab/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/movio/moviolab/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
-package com.movio.moviolab.controllers;
+package com.movio.moviolab.exceptions;
 
-import com.movio.moviolab.exceptions.MovieNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
---
+++ src/main/java/com/movio/moviolab/services/MovieService.java
@@ -0,0 +1,50 @@
+package com.movio.moviolab.services;
+
+import com.movio.moviolab.exceptions.MovieNotFoundException;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+    private final List<Movie> movies = List.of(
+            new Movie("Inception", Movie.GENRE_SCIFI, 2010),
+            new Movie("Titanic", Movie.GENRE_ROMANCE, 1997),
+            new Movie("Leon: The Professional", Movie.GENRE_ACTION, 1994),
+            new Movie("The Terminator", Movie.GENRE_SCIFI, 1984),
+            new Movie("The Exorcist", Movie.GENRE_HORROR, 1973),
+            new Movie("Lost In Translation", Movie.GENRE_COMEDY, 2003),
+            new Movie("E.T. The Extra Terrestrial", Movie.GENRE_ADVENTURE, 1982),
+            new Movie("In The Mood For Love", Movie.GENRE_DRAMA, 2000),
+            new Movie("Forrest Gump", Movie.GENRE_DRAMA, 1994),
+            new Movie("Spirited Away", Movie.GENRE_ADVENTURE, 2001),
+            new Movie("Ghostbusters", Movie.GENRE_ACTION, 1984),
+            new Movie("The Lord Of The Rings", Movie.GENRE_FANTASY, 2001),
+            new Movie("Back To The Future", Movie.GENRE_ADVENTURE, 1985),
+            new Movie("Brokeback Mountain", Movie.GENRE_DRAMA, 2005),
+            new Movie("Paddington 2", Movie.GENRE_ADVENTURE, 2017),
+            new Movie("The Matrix", Movie.GENRE_SCIFI, 1999),
+            new Movie("Reservoir Dogs", Movie.GENRE_CRIME, 1992),
+            new Movie("The Godfather", Movie.GENRE_CRIME, 1972)
+    );
+
+    public List<Movie> getMovies(String genre, Integer year) {
+        List<Movie> filteredMovies = movies.stream()
+                .filter(movie -> (genre == null
+                        || movie.getGenre().equalsIgnoreCase(genre))
+                        && (year == null || movie.getYear().equals(year)))
+                .toList();
+
+        if (filteredMovies.isEmpty()) {
+            throw new MovieNotFoundException("No movies found with the given parameters.");
+        }
+
+        return filteredMovies;
+    }
+
+    public Movie getMovieByMovieName(String movieName) {
+        return movies.stream()
+                .filter(movie -> movie.getMovieName().equalsIgnoreCase(movieName))
+                .findFirst()
+                .orElseThrow(() -> new MovieNotFoundException("Movie not found: " + movieName));
+    }
+}
